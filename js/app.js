1180bbb0019ec60c2053a56c7ac935ea36348766
vlad-vasch-ukr <vasch_ukr@ukr.net>
2020-01-25 22:18:59 +0200

    create section models

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready (function(){
+    //hero
+ 
     $(window).on('resize', function(){
         if ($(this).width() > 991) {
             $('.ba-header-nav').show();
@@ -16,8 +18,45 @@ $(document).ready (function(){
         //autoplay: true,
         arrows: true,
         dots: true,
+        fade: true
     });
 
     $('.slick-dots li button').prepend('0');
     $(".ba-hero ul.slick-dots").wrap("<div class='container dots-wrapper'></div>");
+
+    new WOW().init();
+    //models
+
+
+    $('.ba-models-slider').slick({
+        speed: 3000,
+        //autoplay: true,
+        infinite: true,
+        arrows: true,
+        prevArrow: $('.slide-prev'),
+        nextArrow: $('.slide-next'),
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        centerMode: true,
+        mobileFirst: true,
+        //fade: true,
+        //useTransform: true,
+        //adaptiveHeight: true,
+        /*responsive: [
+                    {
+                      breakpoint: 765,
+                        settings: {
+                        slidesToShow: 1,
+                        infinite: true,
+                        variableWidth: false,
+                      }
+                    },
+                    ]*/
+    });
+
+    $('.ba-section-models .slick-dots li button').prepend('0');
+    $(".ba-section-models ul.slick-dots").wrap("<div class='models-dots-wrapper'></div>");
+
 })
